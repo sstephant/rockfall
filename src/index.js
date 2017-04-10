6b582b0f32770868d8a1e0be0d6bd8c5ab093757
Sylvain STEPHANT <ext.sylvain.stephant@itnovem.com>
2017-04-10 19:09:47 +0200

    WIP

--- src/index.js
+++ src/index.js
@@ -2,43 +2,45 @@ const waterfall = require('promise-waterfall');
 
 /**
  * @param {Function<Promise<*>>[]} promiseFactories
+ * @param {Function<Promise<*>>} chain
  * @returns {Function<Promise<*>>}
  */
-const batch = promiseFactories => (
-  () => (Promise.all(promiseFactories.map(pf => (pf()))))
+const batch = (promiseFactories, chain = Promise.resolve) => (
+  () => (Promise.all(promiseFactories.map(pf => (pf()))).then(chain))
 );
+
 /**
  *
  * @param {Function<Promise<*>>[]} promiseFactories an array of functions returning a promise
  * @param {Number} batchSize integer expected to be >= 1
- * @returns {*}
+ * @returns {Promise}
  */
-const rockfall = (promiseFactories = [], batchSize = 1) => {
-  const size = parseInt(batchSize, 10);
+const rockfall = (promiseFactories = [], batchSize = 10) => {
+  let size = parseInt(batchSize, 10);
   if (!Array.isArray(promiseFactories) || promiseFactories.length === 0) {
     return Promise.resolve([]);
   }
-  if (isNaN(size) || size <= 1) {
-    return waterfall(promiseFactories);
-  }
-  if (size >= promiseFactories.length) {
-    return batch(promiseFactories)();
+  if (isNaN(size) || size < 1) {
+    size = 1;
   }
   const batches = [];
+  const allPromises = [];
+  const chain = (rs) => {
+    return Promise.resolve(rs);
+  };
   let currentBatch = [];
   promiseFactories.forEach((pf) => {
     if (currentBatch.length === size) {
-      batches.push(batch(currentBatch));
+      batches.push(batch(currentBatch, ));
       currentBatch = [];
     }
     currentBatch.push(pf);
   });
   batches.push(batch(currentBatch));
-  return waterfall(batches);
+  return waterfall(batches).then(() => {
+    return Promise.all(allPromises);
+  });
 };
 
-/**
- *
- */
 module.exports = rockfall;
 module.exports.batch = batch;
