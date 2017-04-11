2906fa4e310c928930559c361ce3a5989e769e7a
Sylvain <sstephant+github@gmail.com>
2017-04-11 11:27:54 +0200

    WIP

--- src/index.js
+++ src/index.js
@@ -1,21 +1,31 @@
 const waterfall = require('promise-waterfall');
+const debug = require('debug');
+
+const trace = debug('rockfall');
+
+const identity = rs => (Promise.resolve(rs));
+const tracer = (n, then) => (rs) => {
+  trace(`batch ${n} is resolved`);
+  return then(rs);
+};
 
 /**
  * @param {Function<Promise<*>>[]} promiseFactories
- * @param {Function<Promise<*>>} chain
+ * @param {Function<Promise<*>>} batchThen
  * @returns {Function<Promise<*>>}
  */
-const batch = (promiseFactories, chain = Promise.resolve) => (
-  () => (Promise.all(promiseFactories.map(pf => (pf()))).then(chain))
+const batch = (promiseFactories, batchThen = identity) => (
+  () => (Promise.all(promiseFactories.map(pf => (pf()))).then(batchThen))
 );
 
 /**
  *
  * @param {Function<Promise<*>>[]} promiseFactories an array of functions returning a promise
  * @param {Number} batchSize integer expected to be >= 1
+ * @param {Function} batchThen
  * @returns {Promise}
  */
-const rockfall = (promiseFactories = [], batchSize = 10) => {
+const rockfall = (promiseFactories = [], batchSize = 10, batchThen = identity) => {
   let size = parseInt(batchSize, 10);
   if (!Array.isArray(promiseFactories) || promiseFactories.length === 0) {
     return Promise.resolve([]);
@@ -24,22 +34,16 @@ const rockfall = (promiseFactories = [], batchSize = 10) => {
     size = 1;
   }
   const batches = [];
-  const allPromises = [];
-  const chain = (rs) => {
-    return Promise.resolve(rs);
-  };
   let currentBatch = [];
   promiseFactories.forEach((pf) => {
     if (currentBatch.length === size) {
-      batches.push(batch(currentBatch, ));
+      batches.push(batch(currentBatch, tracer(batches.length, batchThen)));
       currentBatch = [];
     }
     currentBatch.push(pf);
   });
-  batches.push(batch(currentBatch));
-  return waterfall(batches).then(() => {
-    return Promise.all(allPromises);
-  });
+  batches.push(batch(currentBatch, tracer(batches.length, batchThen)));
+  return waterfall(batches);
 };
 
 module.exports = rockfall;
--- test/rockfallSpec.js
+++ test/rockfallSpec.js
@@ -1,24 +1,27 @@
 const chai = require('chai');
+const debug = require('debug');
 const rockfall = require('../src/index');
 
+const trace = debug('test:rockfall');
 const expect = chai.expect;
 
 describe('promise-rockfall', () => {
   const promiseFactories = Array(100).fill(Math.random()).map(n => () => (Promise.resolve(n)));
   const batchSize = 10;
+  trace(`${promiseFactories.length} promises to be resolved in chunk of ${batchSize}`);
   expect(rockfall).to.be.a.function;
   it('should return a promise', () => {
     expect(rockfall([])).to.be.a('promise');
-    const myBatchifiedPromise = rockfall(promiseFactories, batchSize);
-    expect(myBatchifiedPromise).to.be.a('promise');
-    return myBatchifiedPromise.then((rs) => {
+    const myBatchifiedPromises = rockfall(promiseFactories, batchSize);
+    return myBatchifiedPromises.then((rs) => {
+      trace(rs.length);
       expect(rs).to.be.an.array;
       expect(rs).to.have.length.within(1, batchSize);
     });
   });
   describe('batch()', () => {
     expect(rockfall).to.have.property('batch');
-    it('should return a function which returns an array of promises', () => {
+    it('should return a function which returns a promise', () => {
       const f = rockfall.batch(promiseFactories);
       expect(f).to.be.a.function;
       expect(f()).to.be.a('promise');
